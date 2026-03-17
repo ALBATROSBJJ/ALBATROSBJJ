458e64ae5a43aab9ad8b94f5075692c11fc498a2
albatros <albatrostaekwondo@gmail.com>
2026-03-17 04:00:42 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7246712953-d1134",
+  "appId": "1:370604842001:web:ce9019ba6bf907cd5865b0",
+  "apiKey": "AIzaSyDwP9wWyogdTgl1oM6RTvvuxT8Of4pWEuo",
+  "authDomain": "studio-7246712953-d1134.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "370604842001"
+};
