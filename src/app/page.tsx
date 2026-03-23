be8735874368bb1515149e7908e04db07d2541e4
albatros <albatrostaekwondo@gmail.com>
2026-03-23 04:26:41 +0000

    cambia a "bjj.png"  (_for element img_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -95,7 +95,7 @@ export default function WelcomePage() {
           className="h-screen flex items-center justify-center relative overflow-hidden"
         >
           <Image
-            src="/Mibaner.png"
+            src="/bjj.png"
             alt="Banner de Albatros"
             fill
             className="object-cover z-0"
@@ -139,7 +139,7 @@ export default function WelcomePage() {
           className="min-h-screen flex items-center py-20 relative"
         >
           <Image
-            src="/Mibaner.png"
+            src="/bjj.png"
             alt="Nuestros Servicios"
             fill
             className="object-cover z-0"
